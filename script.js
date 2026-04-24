450b7cc68469931c7635fc25c883f8a5f196594d
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2026-04-23 22:05:58 -0400

    functionality added
    able to add pokemon and fakeapi api

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-console.log('Hello from script.js');
+// console.log('Hello from script.js');
+
+const container = document.getElementById('data-container');
+
 
 
 async function callApp() {
 
-let res = await fetch('https://jsonplaceholder.typicode.com/posts') 
-let data =res.json()
+let res = await fetch('https://jsonplaceholder.typicode.com/users') 
+let data = await res.json()
+
+let pokeRes = await fetch('https://pokeapi.co/api/v2/pokemon/poliwag');
+let pokeData = await pokeRes.json();
      
-  
+container.innerHTML = `
+        User: ${data[0].name} <br> 
+  Pokemon:
+  ${pokeData.name}
+  <br> 
+  <img src="${pokeData.sprites.back_default}">
+        
+    `;
+  // console.log(data[0].name);
+  // console.log(pokeData.sprites.back_default);
   return data
 }
-console.log(callApp())
+  callApp()
 
 
 //need two apis
 
-https://pokeapi.co/
+// https://pokeapi.co/
