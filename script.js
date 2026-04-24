85fae74b6db15b8f3f38f89ad334aae5c606fe78
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2026-04-23 20:25:00 -0400

    feat: add basic fetch function for JSONPlaceholder API
    Added callApp: An async function to fetch and return post data.
    
    Console logs: Added initial script check and function call output.
    
    Documentation: Noted requirement for a second API (PokéAPI).

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello from script.js');
+
+
+async function callApp() {
+
+let res = await fetch('https://jsonplaceholder.typicode.com/posts') 
+let data =res.json()
+     
+  
+  return data
+}
+console.log(callApp())
+
+
+//need two apis
+
+https://pokeapi.co/
